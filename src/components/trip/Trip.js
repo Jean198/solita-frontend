340e6e875dff33c00c763ae578063559f0be4646
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-12-29 10:35:54 +0200

    structured the project

---
+++ src/components/trip/Trip.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './trip.css'
+
+const Trip = () => {
+  return (
+    <div>Trip</div>
+  )
+}
+
+export default Trip
---
+++ src/components/tripsList/TripsList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './tripsList.css'
+
+const TripsList = () => {
+  return (
+    <div>TripsList</div>
+  )
+}
+
+export default TripsList
