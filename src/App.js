bff5e3de0f3672ffeec94a64373113f58bfe1116
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-12-29 10:53:20 +0200

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from './components/navBar/NavBar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="container-fluid">
+        <NavBar/>
 
-    </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './navBar.css'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Helsinki City Bikes
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarTogglerDemo01"
+              aria-controls="navbarTogglerDemo01"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link active">All Trips</Link>
+                </li>
+                <li className="nav-item">
+                <Link to="/notes" className="nav-link active">Stations</Link>
+                </li>
+                <li className="nav-item">
+                <Link to="/create" className="nav-link active">Stations Map</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
   )
 }
 
