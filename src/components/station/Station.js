2ee0164dae6caa0478957f84e5e9d526f17e886e
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-12-30 19:38:36 +0200

    fetched the departure from and return at Counts

--- src/components/station/Station.js
+++ src/components/station/Station.js
@@ -1,16 +1,17 @@
 import "./station.css";
 import React from "react";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+
 
 const Station = ({ station,index }) => {
   const navigate = useNavigate();
   const handleRowClick = (id) => {
     navigate(`/stations/station/${station.id}`);
   };
+
   return (
     <>
-        <tr onClick={()=>handleRowClick(station.id)} className="station-row" key={index}>
+        <tr onClick={()=>{handleRowClick(station.id)}} className="station-row" key={index}>
           <td></td>
           <td>{station.FID}</td>
           <td>{station.name}</td>
--- src/components/stationDetails/StationDetails.js
+++ src/components/stationDetails/StationDetails.js
@@ -1,21 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import StationsContext from "../../context/StationsContext";
-import StationsList from "../stationsList/StationsList";
 import './stationDetails.css'
+import axios from 'axios'
 
 const StationDetails = () => {
   const stastionsList = useContext(StationsContext);
-  console.log(stastionsList[0]);
+  const[stationsOccurencesCount, setStationsOccurencesCount]=useState([]);
   let { id } = useParams();
+
+  const countStationsOccurences= async(id)=>{
+
+    try {
+      await axios.get(`http://localhost:5000/stations/station/${id}`).then((response) => {
+        console.log(response.data)
+        setStationsOccurencesCount(response.data);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  useEffect(()=>{
+    countStationsOccurences(id)
+  },[])
+
+
   return (
     <div className="station-details-container">
       {stastionsList
         .filter((station) => station.id === id)
         .map((station) => {
           return (<>
-          <h3>{station.name}</h3>
-          <p>-{station.address}</p>
+          <h2>{station.name}</h2>
+          <p><b>Address:</b> {station.address}</p>
+          <p>Number of Journeys starting from this Station: <b>{stationsOccurencesCount.departureCounts}</b></p>
+          <p>Number of Journeys Ending at this Station: <b>{stationsOccurencesCount.returnCounts}</b></p>
           </>);
         })}
     </div>
