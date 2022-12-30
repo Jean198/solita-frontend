08d253a189bc7ea5542a86d8e7be1c47b47da054
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-12-30 09:18:09 +0200

    added pagination and styles

--- src/components/trip/Trip.js
+++ src/components/trip/Trip.js
@@ -9,7 +9,7 @@ const Trip = ({trip}) => {
       <td>{trip.departure_station_id}</td>
       <td>{trip.return_station_name}</td>
       <td>{trip.return_station_id}</td>
-      <td>{(trip.covered_distance_m/1000).toFixed(6)}</td>
+      <td>{(trip.covered_distance_m/1000).toFixed(2)}</td>
       <td>{(trip.return_station_id/60).toFixed(1)}</td>
     </tr>
     </>
--- src/components/tripsList/TripsList.js
+++ src/components/tripsList/TripsList.js
@@ -1,12 +1,11 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./tripsList.css";
-import "../searchForm/SearchForm"
-import bike_img from '../../assets/images/bikes-img.jpg'
+import "../searchForm/SearchForm";
+import bike_img from "../../assets/images/bikes-img.jpg";
 import SearchForm from "../searchForm/SearchForm";
 import Trip from "../trip/Trip";
-import axios from 'axios'
+import axios from "axios";
 import ReactPaginate from "react-paginate";
-
 
 const TripsList = () => {
   const [tripsData, setTripsData] = useState([]);
@@ -15,16 +14,16 @@ const TripsList = () => {
   const [numberOfPages, setNumberOfPages] = useState(0);
   const [totalRows, setTotalRows] = useState(0);
   const [searchString, setSearchString] = useState("");
-  const [searchType, setSearchType]=useState("departure_station_id")
+  const [searchType, setSearchType] = useState("departure_station_id");
 
   const getTrips = async () => {
-    console.log(searchString)
+    console.log(searchString);
     const response = await axios
       .get(
         `http://localhost:5000?limit=${limit}&page=${pageNumber}&search=${searchString}&searchType=${searchType}`
       )
       .then((response) => {
-        console.log(response.data.data)
+        console.log(response.data.data);
         setTripsData(response.data.data);
         setTotalRows(response.data.paging.total);
         setPageNumber(response.data.paging.page);
@@ -33,27 +32,33 @@ const TripsList = () => {
   };
 
   const handleSearch = (e) => {
-    console.log(e.target.value)
+    console.log(e.target.value);
     setSearchString(e.target.value);
   };
 
-  const handleOptionsChange=(e)=>{
-    console.log(e.target.value)
-    setSearchType(e.target.value)
-  }
+  const handleOptionsChange = (e) => {
+    console.log(e.target.value);
+    setSearchType(e.target.value);
+  };
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   useEffect(() => {
     getTrips();
   }, [pageNumber, searchString]);
 
-
-
   return (
     <div className="container">
       <div>
-        <img src={bike_img} alt="" className="bikes-img"/>
+        <img src={bike_img} alt="" className="bikes-img" />
       </div>
-      <SearchForm handleSearch={handleSearch} searchString={searchString} handleOptionsChange={handleOptionsChange}/>
+      <SearchForm
+        handleSearch={handleSearch}
+        searchString={searchString}
+        handleOptionsChange={handleOptionsChange}
+      />
       <div className="table-responsive mt-3">
         <table className=" table">
           <thead className="table-head ">
@@ -68,19 +73,35 @@ const TripsList = () => {
             </tr>
           </thead>
           <tbody>
-
             {tripsData &&
               tripsData.map((trip, index) => {
-                return (
-                  <Trip trip={trip} index={index}/>
-                );
+                return <Trip trip={trip} index={index} />;
               })}
           </tbody>
         </table>
-        <p className="data-statistics">
-            Total Rows: <b>{totalRows}</b> &nbsp;&nbsp;&nbsp; Page: <b>{totalRows ? pageNumber + 1 : null}</b> of{" "}
-            <b>{numberOfPages}</b>
-          </p>
+        <div className="row mt-5">
+          <div className="col-lg-4">
+            <p className="data-statistics">
+              Total Rows: <b>{totalRows}</b> &nbsp;&nbsp;&nbsp; Page:{" "}
+              <b>{totalRows ? pageNumber + 1 : null}</b> of{" "}
+              <b>{numberOfPages}</b>
+            </p>
+          </div>
+          <div className="col-lg-8">
+            <ReactPaginate
+              previousLabel={"<<"}
+              nextLabel={">>"}
+              pageCount={numberOfPages}
+              onPageChange={changePage}
+              containerClassName={"pagination-list"}
+              pageLinkClassName={"btn "}
+              previousClassName={"btn btn-info"}
+              nextClassName={"btn btn-info"}
+              activeLinkClassName={"btn btn-success"}
+              disabledClassName={"btn btn-light"}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
