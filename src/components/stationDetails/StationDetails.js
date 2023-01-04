f454f36faf306e8dc77f731a9b5d83c6f9be6d9a
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-01-04 10:53:10 +0200

    showing popular stations for single station view

--- src/components/stationDetails/StationDetails.js
+++ src/components/stationDetails/StationDetails.js
@@ -1,44 +1,126 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import StationsContext from "../../context/StationsContext";
-import './stationDetails.css'
-import axios from 'axios'
-
+import "./stationDetails.css";
+import axios from "axios";
 
 const StationDetails = () => {
   const data = useContext(StationsContext);
-  const stationsList=data.data
-  const[stationsOccurencesCount, setStationsOccurencesCount]=useState([]);
+  const stationsList = data.data;
+  const [singleStationData, setsingleStationData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   let { id } = useParams();
 
-  const countStationsOccurences= async(id)=>{
-
+  const countStationsOccurences = async (id) => {
+    setLoading(true);
     try {
-      await axios.get(`http://localhost:5000/stations/station/${id}`).then((response) => {
-        setStationsOccurencesCount(response.data);
-      });
+      await axios
+        .get(`http://localhost:5000/stations/station/${id}`)
+        .then((response) => {
+          setsingleStationData(response.data);
+          setLoading(false);
+        });
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
-  useEffect(()=>{
-    countStationsOccurences(id)
-  },[])
-
+  useEffect(() => {
+    countStationsOccurences(id);
+  }, []);
 
   return (
     <div className="station-details-container">
-      {stationsList
-        .filter((station) => station.id === id)
-        .map((station) => {
-          return (<>
-          <h1>{station.name}</h1>
-          <p><b>Address:</b> <span className="address">{station.address}</span></p>
-          <p>Number of Journeys starting from this Station: <b>{stationsOccurencesCount.departureCounts}</b></p>
-          <p>Number of Journeys Ending at this Station: <b>{stationsOccurencesCount.returnCounts}</b></p>
-          </>);
-        })}
+      {stationsList &&
+        stationsList
+          .filter((station) => station.id === id)
+          .map((station, index) => {
+            return (
+              <>
+                <h1>{station.name}</h1>
+                <div key={index} className="row station-box">
+                  <p>
+                    <b>Address:</b>{" "}
+                    <span className="address">{station.address}</span>
+                  </p>
+                  <p>
+                    Number of Journeys starting from this Station:{" "}
+                    <b>{singleStationData.departureCounts}</b>
+                  </p>
+                  <p>
+                    Number of Journeys Ending at this Station:{" "}
+                    <b>{singleStationData.returnCounts}</b>
+                  </p>
+                </div>
+
+                <div className="row text-center station-box mt-3">
+                  <h5 className="mb-5">Popular stations</h5>
+                  <div className="col-lg-6 text-center">
+                    <div className="mb-5">
+                      <b>
+                      Popular departure stations for trips ending at{" "}
+                        <span className="address">{station.name}</span>
+                      </b>
+                    </div>
+
+                    {loading ? (
+                      <div className="loading">
+                        <b>Please wait while fetching...</b>
+                      </div>
+                    ) : null}
+
+                    {singleStationData.popularDepartureStations &&
+                      singleStationData.popularDepartureStations.map(
+                        (station, index) => {
+                          return (
+                            <div key={index} className="">
+                              <p>
+                                {index + 1}. {station._id}(Departures:{" "}
+                                {station.count})
+                              </p>
+                              <p></p>
+                              <hr />
+                            </div>
+                          );
+                        }
+                      )}
+                  </div>
+
+                  <div className="col-lg-6 text-center">
+                    <div className="mb-5">
+                      <b>
+                        Popular return stations for trips starting at{" "}
+                        <span className="address">{station.name}</span>
+                      </b>
+                    </div>
+
+                    {loading ? (
+                      <div className="loading">
+                        <b>Please wait while fetching...</b>
+                      </div>
+                    ) : null}
+
+                    {singleStationData.popularReturnStations &&
+                      singleStationData.popularReturnStations.map(
+                        (station, index) => {
+                          return (
+                            <div key={index}>
+                              <p>
+                                {index + 1}. {station._id}(Returns:{" "}
+                                {station.count})
+                              </p>
+                              <p></p>
+                              <hr />
+                            </div>
+                          );
+                        }
+                      )}
+                  </div>
+                </div>
+              </>
+            );
+          })}
     </div>
   );
 };
