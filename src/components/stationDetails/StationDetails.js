5ad2fc7101fa0827983b43e1deb901930a8fcb57
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-01-04 12:15:05 +0200

    added average distances

--- src/components/stationDetails/StationDetails.js
+++ src/components/stationDetails/StationDetails.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import StationsContext from "../../context/StationsContext";
 import "./stationDetails.css";
 import axios from "axios";
-import loader from "../../assets/images/loader.gif"
+import loader from "../../assets/images/loader.gif";
 
 const StationDetails = () => {
   const data = useContext(StationsContext);
@@ -38,29 +38,49 @@ const StationDetails = () => {
           .filter((station) => station.id === id)
           .map((station, index) => {
             return (
-              <>
+              <div key={index}>
                 <h1>{station.name}</h1>
-                <div key={index} className="row station-box">
-                  <p>
-                    <b>Address:</b>{" "}
-                    <span className="address">{station.address}</span>
-                  </p>
-                  <p>
-                    Number of Journeys starting from this Station:{" "}
-                    <b>{singleStationData.departureCounts}</b>
-                  </p>
-                  <p>
-                    Number of Journeys Ending at this Station:{" "}
-                    <b>{singleStationData.returnCounts}</b>
-                  </p>
+                <div  className="row station-box">
+                  <ul>
+                    <li>
+                      <b>Address:</b>{" "}
+                      <span className="address">{station.address}</span>
+                      <ul className="mt-3">
+                        <li>
+                          Number of trips starting from this Station:{" "}
+                          <b>{singleStationData.departureCounts}</b>
+                        </li>
+                        <li>
+                          Number of trips Ending at this Station:{" "}
+                          <b>{singleStationData.returnCounts}</b>
+                        </li>
+                        <li>
+                          Average distance of trips starting from this station:{" "}
+                          <b>
+                            {singleStationData.averageDepartureDistance && singleStationData.averageDepartureDistance.toFixed(
+                              2
+                            )}
+                          </b>{" "}
+                          meters
+                        </li>
+                        <li>
+                          Average distance of trips ending at this station:{" "}
+                          <b>
+                            {singleStationData.averageReturnDistance && singleStationData.averageReturnDistance.toFixed(2)}
+                          </b>{" "}
+                          meters
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
                 </div>
 
-                <div className="row text-center station-box mt-3">
-                  <h5 className="mb-5">Popular stations</h5>
-                  <div className="col-lg-6 text-center">
+                <div className="row  station-box mt-3">
+                  <h3 className="mb-5">Popular stations</h3>
+                  <div className="col-lg-6">
                     <div className="mb-5">
                       <b>
-                      Popular departure stations for trips ending at{" "}
+                        Popular departure stations for trips ending at{" "}
                         <span className="address">{station.name}</span>
                       </b>
                     </div>
@@ -88,7 +108,7 @@ const StationDetails = () => {
                       )}
                   </div>
 
-                  <div className="col-lg-6 text-center">
+                  <div className="col-lg-6">
                     <div className="mb-5">
                       <b>
                         Popular return stations for trips starting at{" "}
@@ -119,7 +139,7 @@ const StationDetails = () => {
                       )}
                   </div>
                 </div>
-              </>
+              </div>
             );
           })}
     </div>
